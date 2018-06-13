c5b68379189f61abe47fd6483897c43f86efaca6
michaelmcmullin <m.mcmullin@outlook.com>
2018-06-13 07:13:56 +0100

    First commit

---
+++ csMatrix.Tests/MatrixTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace csMatrix.Tests
+{
+    public class MatrixTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ csMatrix/Matrix.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace csMatrix
+{
+    public class Matrix
+    {
+    }
+}
