c60bd9f1b41c58cd83ce97ee091863e7b20d58b2
michaelmcmullin <m.mcmullin@outlook.com>
2018-06-14 07:09:49 +0100

    Adds Matrix class outline with failing tests

--- csMatrix.Tests/MatrixTests.cs
+++ csMatrix.Tests/MatrixTests.cs
@@ -6,9 +6,74 @@ namespace csMatrix.Tests
     public class MatrixTests
     {
         [Fact]
-        public void Test1()
+        public void MatrixConstructorIntInt()
         {
+            int rows = 3, columns = 5;
+            Matrix m = new Matrix(rows, columns);
+            Assert.Equal(rows, m.Rows);
+            Assert.Equal(columns, m.Columns);
+        }
 
+        [Fact]
+        public void MatrixConstructorIntIntDouble()
+        {
+            int rows = 3, columns = 5;
+            double value = 3.14;
+            Matrix m = new Matrix(rows, columns, value);
+            Assert.Equal(rows, m.Rows);
+            Assert.Equal(columns, m.Columns);
+            Assert.Equal(value, m[0, 0]);
+            Assert.Equal(value, m[rows - 1, columns - 1]);
+            Assert.Equal(value, m[1, 1]);
+        }
+
+        [Fact]
+        public void MatrixConstructorInt()
+        {
+            int rows = 3;
+            Matrix m = new Matrix(rows, rows);
+            Assert.Equal(rows, m.Rows);
+            Assert.Equal(rows, m.Columns);
+        }
+
+        [Fact]
+        public void MatrixConstructorIntDouble()
+        {
+            int rows = 3;
+            double value = 3.14;
+            Matrix m = new Matrix(rows, rows, value);
+            Assert.Equal(rows, m.Rows);
+            Assert.Equal(rows, m.Columns);
+            Assert.Equal(value, m[0, 0]);
+            Assert.Equal(value, m[rows - 1, rows - 1]);
+            Assert.Equal(value, m[1, 1]);
+        }
+
+        [Fact]
+        public void MatrixConstructorDoubleArray()
+        {
+            double[,] arr = { { 1, 2, 3 }, { 4, 5, 6 } };
+            int rows = arr.GetLength(0);
+            int columns = arr.GetLength(1);
+            Matrix m = new Matrix(arr);
+            Assert.Equal(rows, m.Rows);
+            Assert.Equal(columns, m.Columns);
+            Assert.Equal(arr[0,0], m[0, 0]);
+            Assert.Equal(arr[rows - 1, columns - 1], m[rows - 1, columns - 1]);
+        }
+
+        [Fact]
+        public void MatrixConstructorMatrix()
+        {
+            int rows = 3, columns = 2;
+            double value = 3.14;
+            Matrix m1 = new Matrix(rows, columns, value);
+            Matrix m2 = new Matrix(m1);
+            Assert.Equal(rows, m2.Rows);
+            Assert.Equal(columns, m2.Columns);
+            Assert.Equal(value, m2[0, 0]);
+            Assert.Equal(value, m2[rows - 1, columns - 1]);
+            Assert.Equal(value, m2[1, 1]);
         }
     }
 }
--- csMatrix/Matrix.cs
+++ csMatrix/Matrix.cs
@@ -1,8 +1,149 @@
 ﻿using System;
+using System.Collections;
 
 namespace csMatrix
 {
-    public class Matrix
+    /// <summary>
+    /// A Matrix class for C#
+    /// </summary>
+    public class Matrix : IEnumerable
     {
+        #region Fields
+        /// <summary>
+        /// Storage array for the Matrix data.
+        /// </summary>
+        double[] data;
+
+        /// <summary>
+        /// Number of rows in this Matrix
+        /// </summary>
+        int rows;
+
+        /// <summary>
+        /// Number of columns in this Matrix
+        /// </summary>
+        int columns;
+
+        /// <summary>
+        /// Number of elements in this Matrix
+        /// </summary>
+        int size;
+
+        /// <summary>
+        /// Number of rows in this Matrix
+        /// </summary>
+        int[] dimensions;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Constructor to create a new Matrix while specifying the number of rows and columns.
+        /// </summary>
+        /// <param name="rows">The number of rows to initialise the Matrix with.</param>
+        /// <param name="columns">The number of columns to initialise the Matrix with.</param>
+        public Matrix(int rows, int columns)
+        {
+        }
+
+        /// <summary>
+        /// Constructor to create a new Matrix while specifying the number of rows,
+        /// columns and default values.
+        /// </summary>
+        /// <param name="rows">The number of rows to initialise the Matrix with.</param>
+        /// <param name="cols">The number of columns to initialise the Matrix with.</param>
+        /// <param name="value">The value to fill the Matrix with.</param>
+        public Matrix(int rows, int columns, double value) : this(rows, columns)
+        {
+        }
+
+        /// <summary>
+        /// Constructor to create a new square Matrix.
+        /// </summary>
+        /// <param name="dimensions">The number of rows and columns to initialise the
+        /// Matrix with. There will be an equal number of rows and columns.</param>
+        public Matrix(int dimensions) : this(dimensions, dimensions)
+        {
+        }
+
+        /// <summary>
+        /// Constructor to create a new square Matrix filled with a default value.
+        /// </summary>
+        /// <param name="dimensions">The number of rows and columns to initialise the
+        /// Matrix with. There will be an equal number of rows and columns.</param>
+        /// <param name="value">The value to fill the Matrix with.</param>
+        public Matrix(int dimensions, double value) : this(dimensions, dimensions, value)
+        {
+        }
+
+        /// <summary>
+        /// Constructor to create a new Matrix based on an existing two-dimensional array.
+        /// </summary>
+        /// <param name="array">The array to specify values for a new Matrix.</param>
+        public Matrix(double[,] array) : this(array.GetLength(0), array.GetLength(1))
+        {
+        }
+
+        /// <summary>
+        /// Constructor to create a new Matrix based on an existing Matrix.
+        /// </summary>
+        /// <param name="m">The existing Matrix to specify values for a new Matrix.</param>
+        public Matrix(Matrix m) : this(m.Rows, m.Columns)
+        {
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Indicates whether or not this Matrix row and column dimensions are equal.
+        /// </summary>
+        public bool IsSquare => Rows == Columns;
+
+        /// <summary>
+        /// Get the dimensions of this Matrix in a single-dimensional array of the form
+        /// [rows,columns].
+        /// </summary>
+        public int[] Dimensions => dimensions;
+
+        /// <summary>
+        /// Get the number of rows in this Matrix.
+        /// </summary>
+        public int Rows => rows;
+
+        /// <summary>
+        /// Get the number of columns in this Matrix.
+        /// </summary>
+        public int Columns => columns;
+
+        /// <summary>
+        /// Get the total number of elements in this Matrix.
+        /// </summary>
+        public int Size => size;
+        #endregion
+
+        #region Indexers
+        /// <summary>
+        /// Indexer to easily access a specific location in this Matrix.
+        /// </summary>
+        /// <param name="row">The row of the Matrix location to access.</param>
+        /// <param name="column">The column of the Matrix location to access.</param>
+        /// <returns>The value stored at the given row/column location.</returns>
+        /// <remarks>Matrices are zero-indexed.</remarks>
+        public double this[int row, int column]
+        {
+            get { return data[(row * Columns) + column]; }
+            set { data[(row * Columns) + column] = value; }
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Implement the GetEnumerator method to run against the data array.
+        /// </summary>
+        /// <returns>Returns an enumerator for the data array.</returns>
+        public IEnumerator GetEnumerator()
+        {
+            return data.GetEnumerator();
+        }
+        #endregion
     }
 }
