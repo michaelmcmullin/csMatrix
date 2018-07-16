c4b6370a5b0408fc988e32dccd2a275391c12c70
michaelmcmullin <m.mcmullin@outlook.com>
2018-07-16 06:33:58 +0100

    Adds unit tests for static Inverse method

--- csMatrix.Tests/MatrixTests.cs
+++ csMatrix.Tests/MatrixTests.cs
@@ -858,7 +858,7 @@ namespace csMatrix.Tests
             Matrix m2 = Matrix.Inverse(m1);
             m1.Inverse();
             Assert.Equal(expected, m1);
-            //Assert.Equal(expected, m2);
+            Assert.Equal(expected, m2);
         }
 
         [Fact]
