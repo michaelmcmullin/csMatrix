d866a54c6243a392235dd9d0deaacf96aa598638
michaelmcmullin <m.mcmullin@outlook.com>
2018-07-24 07:36:22 +0100

    Rewrite Reshape test as Theory

--- csMatrix.Tests/MatrixOperationReshapeTests.cs
+++ csMatrix.Tests/MatrixOperationReshapeTests.cs
@@ -5,23 +5,21 @@ namespace csMatrix.Tests
 {
     public class MatrixOperationReshapeTests
     {
-        [Fact]
-        public void MatrixReshapeExtractFirstElement()
+        [Theory]
+        [InlineData(0, 1, 1, new double[] { 1.0 })]
+        public void MatrixReshapeExtractElements(int start, int rows, int columns, double[] result)
         {
             Matrix m1 = Setup.GetTestMatrix1();
-            m1.Reshape(0, 1, 1);
-            Matrix m2 = Matrix.Reshape(Setup.GetTestMatrix1(), 0, 1, 1);
+            m1.Reshape(start, rows, columns);
+            Matrix m2 = Matrix.Reshape(Setup.GetTestMatrix1(), start, rows, columns);
 
-            Matrix expected = new Matrix(new double[,] { { 1.0 } });
+            Matrix expected = new Matrix(rows, columns, result);
 
-            Assert.Equal(1, m1.Size);
-            Assert.Equal(1, m2.Size);
+            Assert.Equal(result.GetLength(0), m1.Size);
+            Assert.Equal(result.GetLength(0), m2.Size);
             Assert.True(m1 == m2);
             Assert.True(m1 == expected);
             Assert.True(m2 == expected);
         }
-
-        //[Theory]
-        //[InlineData(0,1,1,)]
     }
 }
