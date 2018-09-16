0124c542cab7bf4c24b923086b2279fa2c0dfa4d
michaelmcmullin <m.mcmullin@outlook.com>
2018-09-16 10:23:48 +0100

    Adds Bootstrap css

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,20 @@ module.exports = function (grunt) {
                 src: 'src/<%= pkg.name %>.js',
                 dest: 'build/<%= pkg.name %>.min.js'
             }
+        },
+        sass: {
+            dist: {
+                files: {
+                    'css/main.css': 'node_modules/bootstrap/scss/bootstrap.scss'
+                }
+            }
         }
     });
 
-    // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['uglify', 'sass']);
 
 };
